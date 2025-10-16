eeec4f58fdae0f907d4961de9618c772b5c5412f
tgrigoryan-1 <tgrigoryan@ucsd.edu>
2025-10-16 13:37:37 -0700

    Changes made
    Changed some extra things in 131AM and added rm 2 std for both AM and BM

--- mediansplit131AMrm2std.m
+++ mediansplit131AMrm2std.m
@@ -1,4 +1,6 @@
 % Code for MAE131A Spring 25 Median Split vs Performance Analysis Final Exam / Participation After Midterm
+% This code runs the same code as mediansplit131AM.m but excluding two standard deviations above average
+
 % Change the directory to where the csv files are downloaded
 cd('C:\Users\tigrr\Downloads')
 
@@ -11,19 +13,33 @@ g_data = g_data(:,{'SISUserID','FinalExamRawScore_924780_'});
 % Joining the two data sets with the PID's as a key, using innerjoin to drop anyone that is not on both lists
 data = innerjoin(p_data,g_data,'Keys','SISUserID');
 
-% Finding the median participation in total
-medianparticipation = median(p_data.TotalAmount_ActiveRecall_ConceptMapOnly_)
+% Compute mean and std of participation
+mean_score = mean(data.FinalExamRawScore_924780_);
+std_score = std(data.FinalExamRawScore_924780_);
 
-% Setting up split, group above median and group below, where those = median are included in below
-groupAbove = data(data.TotalAmount_ActiveRecall_ConceptMapOnly_ > medianparticipation,:);
-groupBelow = data(data.TotalAmount_ActiveRecall_ConceptMapOnly_ <= medianparticipation,:);
+% Define upper and lower cutoffs (2 standard deviations)
+lower_cutoff = mean_score - 2*std_score;
+upper_cutoff = mean_score + 2*std_score;
 
-% Finding mean, std, median midterm exam score based on split and number in each group
+% Filter out students that are above and below 2 std
+logvec = data.FinalExamRawScore_924780_ >= lower_cutoff & data.FinalExamRawScore_924780_ <= upper_cutoff;
+
+% Remake the data with this filter applied
+datafil = data(logvec,:);
+
+% Define new mediansplit based on filtered cohort
+medianparticipation = median(datafil.TotalAmount_ActiveRecall_ConceptMapOnly_)
+
+% Split the above median group and the below/= median group
+groupAbove = datafil(datafil.TotalAmount_ActiveRecall_ConceptMapOnly_ > medianparticipation,:);
+groupBelow = datafil(datafil.TotalAmount_ActiveRecall_ConceptMapOnly_ <= medianparticipation,:);
+
+% Print values needed
 meanAbove = mean(groupAbove.FinalExamRawScore_924780_)
-meanBelow = mean(groupBelow.FinalExamRawScore_924780_)
 stdAbove = std(groupAbove.FinalExamRawScore_924780_)
-stdBelow = std(groupBelow.FinalExamRawScore_924780_)
 medianAbove = median(groupAbove.FinalExamRawScore_924780_)
+meanBelow = mean(groupBelow.FinalExamRawScore_924780_)
+stdBelow = std(groupBelow.FinalExamRawScore_924780_)
 medianBelow = median(groupBelow.FinalExamRawScore_924780_)
 NAbove = height(groupAbove)
 NBelow = height(groupBelow)
